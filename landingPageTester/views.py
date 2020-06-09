ac6d27407abc6ba5c0140a3a2bd50cb9dad038c5
wintercameearly <oluwanifemi.bamgbose@stu.cu.edu.ng>
2020-06-09 10:42:16 +0100

    chore: Changes to views

--- landingPageTester/views.py
+++ landingPageTester/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+git from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
